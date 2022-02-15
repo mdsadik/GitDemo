249a4d4cb263acdc5322a2256ba5e8c09105bc52
mdsadik <mesadik20@gmail.com>
2022-02-15 00:41:33 -0500

    this is commit from asian timezone guy

--- src/test/java/qaclickacademy/RESTAPITest.java
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -7,6 +7,8 @@ public class RESTAPITest {
 	@Test
 	public void PostJira() {
 		System.out.println("PostJira");
+		System.out.println("PostJira2");
+		System.out.println("PostJira2");
 	}
 	
 	@Test
